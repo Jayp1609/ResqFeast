377bff48eafe3f44d0bf7433a7ab1ef7815e93f5
Jayp1609 <96374272+Jayp1609@users.noreply.github.com>
2024-03-26 12:56:18 +0530

    Update _document.js

--- Client/src/app/_document.js
+++ Client/src/app/_document.js
@@ -1,1 +1,37 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Google Tag Manager */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-BSC3V07QZX"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-BSC3V07QZX');
+              `,
+            }}
+          />
+          {/* End Google Tag Manager */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
 
